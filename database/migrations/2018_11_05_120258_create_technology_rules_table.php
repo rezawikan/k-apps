e97bd034901b9d56a80b9eb3a85e3cca1fae21c3
Mochammad Rezza Wikandito <reza.wikan.dito@gmail.com>
2018-11-29 14:54:59 +0800

    29-11-2018 v.02

--- database/migrations/2018_11_05_120258_create_technology_rules_table.php
+++ database/migrations/2018_11_05_120258_create_technology_rules_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateRolesTechnologiesTable extends Migration
+class CreateTechnologyRulesTable extends Migration
 {
     /**
      * Run the migrations.
