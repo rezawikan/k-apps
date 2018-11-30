1bc4e3e33ff2d0d7094e5624f2b4ff4bc58030c4
Mochammad Rezza Wikandito <reza.wikan.dito@gmail.com>
2018-11-30 15:49:56 +0800

    update 30-11-2018 v.02

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,12 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <meta name="og:title" content="K-Apps"/>
+    <meta name="og:description" content="Internal System of Kopernik"/>
+    <meta name="author" content="Mochammad Rezza Wikandito, reza.wikan.dito@gmail.com">
+    <meta name="owner" content="Kopernik (NGO)">
+    <link rel='fluid-icon' type='image/png' href='https://intranet.kopernik.info/img/k-icon.png'>
+
     <!-- App styles -->
     <link rel="stylesheet" href="{{ mix('css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ mix('font-awesome/css/font-awesome.min.css') }}">
