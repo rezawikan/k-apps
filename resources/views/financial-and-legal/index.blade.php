3e3c8846fd5be63605ae40c6f44815376c550853
= <=>
2019-05-06 12:12:10 +0800

    Revise link on finacial documents

--- resources/views/financial-and-legal/index.blade.php
+++ resources/views/financial-and-legal/index.blade.php
@@ -15,29 +15,93 @@
             <div>
               <ul class="list-group">
                 <li class="list-group-item">
-                  <a href="https://drive.google.com/open?id=0B9WGa9bCW4POLTlHZVktTHlqWkVoampjSFAya25jU1k5VDY0" target="_blank">Yayasan Kopernik Financial Report 2015</a>
+                  <a href="https://drive.google.com/open?id=1fNMB9BC5VpYBW_6Lm4EBYsJtYELFGXgl" target="_blank">Yayasan Kopernik Financial Report 2013</a>
                 </li>
                 <li class="list-group-item">
-                  <a href="https://drive.google.com/file/d/0B_ELp5gNCgJTR0hLSjA4aUNVdDRTOXBEQjV3c19hSTI2Q0tn/view?usp=sharing" target="_blank">Yayasan Kopernik Financial Report 2016</a>
+                  <a href="https://drive.google.com/open?id=1nJDEvKKK7lNygVPI8Pg8hc33Kx7-p9Vh" target="_blank">Yayasan Kopernik Financial Report 2014</a>
                 </li>
                 <li class="list-group-item">
-                  <a href="#">Yayasan Kopernik Financial Report 2017</a>
+                  <a href="https://drive.google.com/open?id=1i35qR1KZ9l7DTJqY5l3URXuBsZOTVqFg" target="_blank">Yayasan Kopernik Financial Report 2015</a>
+                </li>
+                <li class="list-group-item">
+                  <a href="https://drive.google.com/open?id=1H_tgCEI7yJr1F3IQb71s1PZWCTWPzcZo" target="_blank">Yayasan Kopernik Financial Report 2016</a>
+                </li>
+                <li class="list-group-item">
+                  <a href="#" target="_blank">Yayasan Kopernik Financial Report 2017</a>
+                </li>
+                <li class="list-group-item">
+                  &nbsp;
+                </li>
+                <li class="list-group-item">
+                  &nbsp;
+                </li>
+                <li class="list-group-item">
+                  &nbsp;
                 </li>
               </ul>
             </div>
           </div>
           <div class="col-lg-6">
-            <h3>Kopernik Solution</h3>
+            <h3>Kopernik Solutions</h3>
             <div>
               <ul class="list-group">
                 <li class="list-group-item">
-                  <a href="https://v1.kopernik.info/documents/reports/Kopernik%20Audit%20Report%20_%20Financials%202015.pdf" target="_blank">Kopernik Solution Financial Report 2015</a>
+                  <a href="https://drive.google.com/open?id=1Xk7w74co1srzPs3OQfarDhpkwFZw1s96" target="_blank">Kopernik Solution Financial Report 2010</a>
                 </li>
                 <li class="list-group-item">
-                  <a href="https://v1.kopernik.info/documents/reports/Kopernik%20Audit%20Report%20_%20Financials%202016.pdf" target="_blank">Kopernik Solution Financial Report 2016</a>
+                  <a href="https://drive.google.com/open?id=1-XHRWhvlgYrxtjNwjW3fTOvWfVU0LfIn" target="_blank">Kopernik Solution Financial Report 2011</a>
                 </li>
                 <li class="list-group-item">
-                  &nbsp;
+                  <a href="https://drive.google.com/open?id=1g7ZAGgHmlHougVB5SjSFH6uyf6Oy2IA2" target="_blank">Kopernik Solution Financial Report 2012</a>
+                </li>
+                <li class="list-group-item">
+                  <a href="https://drive.google.com/open?id=13qboUtLSZMVIRiWAEJ0PZHcnJZJy-dbH" target="_blank">Kopernik Solution Financial Report 2013</a>
+                </li>
+                <li class="list-group-item">
+                  <a href="https://drive.google.com/open?id=1EbOn_PsDDXl2maFTI5jvDMTDMNEV50zg" target="_blank">Kopernik Solution Financial Report 2014</a>
+                </li>
+                <li class="list-group-item">
+                  <a href="https://drive.google.com/open?id=1PirTKDH44VEOvZFvFErTAM3vj3B8mvcI" target="_blank">Kopernik Solution Financial Report 2015</a>
+                </li>
+                <li class="list-group-item">
+                  <a href="https://drive.google.com/open?id=1wROJj5gAYro6hujjFYZF9ZlPvEWOZglw" target="_blank">Kopernik Solution Financial Report 2016</a>
+                </li>
+                <li class="list-group-item">
+                  <a href="https://drive.google.com/open?id=1D8x1rHIrT1dhPVvmVAsa4zBxc-AcC_9I" target="_blank">Kopernik Solution Financial Report 2017</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <h3>Tax Fillings - Kopernik Solutions</h3>
+            <div>
+              <ul class="list-group">
+                <li class="list-group-item">
+                  <a href="https://drive.google.com/open?id=1XNbeXsxtBtJDVokGgTsAM9-vzZUT7HfS" target="_blank">Kopernik Form 990 2009</a>
+                </li>
+                <li class="list-group-item">
+                  <a href="https://drive.google.com/open?id=1pHlrpGZOvHlQcophqUE9cPKVvsYyMaOd" target="_blank">Kopernik Form 990 2010</a>
+                </li>
+                <li class="list-group-item">
+                  <a href="https://drive.google.com/open?id=1LAJC_7kSUTGXYnEWeV1ObpulPOd6ndw0" target="_blank">Kopernik Form 990 2011</a>
+                </li>
+                <li class="list-group-item">
+                  <a href="https://drive.google.com/open?id=133DywtDTDCV_CEhTGCsciCYbCBZ6lwd0" target="_blank">Kopernik Form 990 2012</a>
+                </li>
+                <li class="list-group-item">
+                  <a href="https://drive.google.com/open?id=1CJN-m5toAZMKnWicM0CTgPefVE_84AAf" target="_blank">Kopernik Form 990 2013</a>
+                </li>
+                <li class="list-group-item">
+                  <a href="https://drive.google.com/open?id=1M5a3aRnVjOVL-pTatLSVV6i-igSvje7k" target="_blank">Kopernik Form 990 2014</a>
+                </li>
+                <li class="list-group-item">
+                  <a href="https://drive.google.com/open?id=1PH_JdOAk9qmRs5vY5TGO6ZpBN25WwGaq" target="_blank">Kopernik Form 990 2015</a>
+                </li>
+                <li class="list-group-item">
+                  <a href="https://drive.google.com/open?id=1H1ThwbCaSEkMJtsizn-qCptdphf3C7kw" target="_blank">Kopernik Form 990 2016</a>
+                </li>
+                <li class="list-group-item">
+                  <a href="https://drive.google.com/open?id=1gF98yPNszmTvmFLpuOXafsckfGU8IMz3" target="_blank">Kopernik Form 990 2017</a>
                 </li>
               </ul>
             </div>
